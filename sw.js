6f2eda376f5f7d08089a5652acbb6bccf263ec7a
AngelosPanagiotou-create <teekal2007@gmail.com>
2026-04-27 12:28:40 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'city-adventure-v2'; // Ανεβάσαμε έκδοση για να ανανεωθεί η μνήμη
+const CACHE_NAME = 'city-adventure-v5'; // Ανεβάσαμε έκδοση για να ανανεωθεί η μνήμη
 const urlsToCache = [
   './',
   './index.html',
